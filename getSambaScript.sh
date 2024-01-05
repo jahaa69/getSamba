0e0bca2be3de3f2df662d082b2bd9edafe50e7a4
jahaa69 <assunnaki@gmail.com>
2024-01-05 19:40:04 +0100

    test user

--- getSambaScript.sh
+++ getSambaScript.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-menu() {
-    echo "$(tput setaf 2)Que faire?"
+menu () {
+    ##je voudrais que le premier echo soit en vert
+    echo "$(tput setaf 2)que fair?"
     echo "$(tput setaf 6)1. Désinstaller Samba"
     echo "2. Ajouter un utilisateur"
     echo "3. Ajouter un disque partagé"
@@ -10,9 +11,9 @@ menu() {
     echo "$(tput setaf 1)6. Quitter"
     echo "$(tput sgr0)"
     read -p "Votre choix : " choix
-    echo "$choix"
 }
 
+# Vérifier si Samba est installé
 if ! command -v smbd &> /dev/null; then
     echo "Samba n'est pas installé. Installation en cours..."
     sudo apt-get update
@@ -26,57 +27,58 @@ if ! command -v smbd &> /dev/null; then
     sudo /etc/init.d/smbd restart
     echo "Samba installé, les dossiers partagés sont dans /home/shares/public."
     menu
+    ## Si Samba est déjà installé, afficher le menu
 else
     menu
 fi
 
 choix=$(menu)
 
-case $choix in
-    1)
-        echo "Désinstallation de Samba..."
-        sudo apt-get remove -y samba samba-common-bin
-        echo "Désinstallation terminée."
-        ;;
-    2)
-        echo "Ajout d'un utilisateur..."
-        read -p "Nom de l'utilisateur : " user
-        sudo useradd $user -m -G users
-        read -p "Mot de passe : " password
-        sudo smbpasswd -a $user
-        echo "$user:$password" >> /etc/samba/smbpasswd
-        echo "Utilisateur ajouté."
-        ;;
-    3)
-        echo "Ajout d'un disque partagé..."
-        read -p "Nom du disque (exemple disk1=sda1 / disk2=sda2): " disk
-        sudo mkdir /home/shares/public/$disk
+if [ $choix -eq 1 ]; then
+    echo "Désinstallation de Samba..."
+    sudo apt-get remove -y samba samba-common-bin
+    echo "Désinstallation terminée."
+    exit 0
+elif [ $choix -eq 2 ]; then
+    echo "Ajout d'un utilisateur..."
+    read -p "Nom de l'utilisateur : " user
+    sudo useradd $user -m -G users
+    read -p "Mot de passe : " password
+    sudo smbpasswd -a $user
+    echo "$user:$password" >> /etc/samba/smbpasswd
+    echo "Utilisateur ajouté."
+    menu
+elif [ $choix -eq 3 ]; then
+    echo "Ajout d'un disque partagé..."
+    read -p "Nom du disque (exemple disk1=sda1 / disk2=sda2): " disk
+    sudo mkdir /home/shares/public/$disk
 
-        # Vérifier si le disque existe
-        if [[ -b /dev/$disk ]]; then
-            if [[ $disk =~ [0-9] ]]; then
-                diskName=$(echo $disk | sed 's/[0-9]*//g')
-                diskNumber=$(echo $disk | sed 's/[^0-9]*//g')
-                sda="sda$diskNumber"
-            fi
-            sudo mount /dev/$sda /home/shares/public/$disk
-
-            echo "/dev/$sda /home/shares/public/$disk ext4 defaults 0 0" | sudo tee -a /etc/fstab
-            echo "Disque partagé ajouté."
-        else
-            echo "Le disque n'existe pas."
+    # Vérifier si le disque existe
+    if [[ -e /dev/$disk ]]; then
+        # Vérifier si le disque a un chiffre dans son nom
+        if [[ $disk =~ [0-9] ]]; then
+            diskName=$(echo $disk | sed 's/[0-9]*//g')
+            diskNumber=$(echo $disk | sed 's/[^0-9]*//g')
+            sda="sda$diskNumber"
         fi
-        ;;
-    4)
-        echo "Liste des dossiers partagés :"
-        sudo ls /home/shares/public
-        ;;
-    5)
-        echo "Liste des utilisateurs et des mots de passe :"
-        sudo cat /etc/samba/smbpasswd
-        ;;
-    *)
-        echo "Script terminé."
-        exit 1
-        ;;
-esac
+        sudo mount /dev/$sda /home/shares/public/$disk
+
+        # Ajouter une entrée dans /etc/fstab
+        echo "/dev/$sda /home/shares/public/$disk ext4 defaults 0 0" | sudo tee -a /etc/fstab
+        echo "Disque partagé ajouté."
+    else
+        echo "Le disque n'existe pas."
+    fi
+    menu
+elif [ $choix -eq 4 ]; then
+    echo "Liste des dossiers partagés :"
+    sudo ls /home/shares/public
+    menu
+elif [ $choix -eq 5 ]; then
+    echo "Liste des utilisateurs et des mots de passe :"
+    sudo cat /etc/samba/smbpasswd
+    menu
+else
+    echo "Script terminé."
+    exit 1
+fi
