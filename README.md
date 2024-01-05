52c5522e0cea4ffdd9c6aa222e9447916a351f09
jahaa69 <assunnaki@gmail.com>
2024-01-05 18:11:59 +0100

    creation du projet + ajout des premier fichier avant test

--- README.md
+++ README.md
@@ -1,2 +1,83 @@
 # getSamba
  it's a small script that allows you to install Samba and configure it automatically
+
+ ## To add another disk 
+ 
+to add another disk you would have to add certain information to do this type the command
+
+
+```bash
+  dmesg	
+```
+
+
+then you will be able to find a new disk surely named sda2 or sd3....
+
+once the name of your disk recover created a new folder
+
+
+```bash
+  sudo mkdir /home/shares/public/disk2
+```
+Then
+
+```bash
+  sudo mount /dev/DISK_NAME /home/shares/public/disk2
+```
+
+
+after you have created the folder entered in the fstab file ATTENTION be careful not to make a mistake or block your raspberry and have to start all over again
+
+```bash
+  sudo nano /etc/fstab	
+```
+once in this file copy and paste this line again don't forget the spaces
+
+```bash
+  /dev/DISK_NAME /home/shares/public/disk2 auto noatime 0 0	
+```
+and ctrl+x , y
+
+finish!!!
+=============================================
+# getSamba
+ c'est un petit script qui permet d'installer Samba et de le configurer automatiquement
+
+ ## Pour ajouter un autre disque
+ 
+pour ajouter un autre disque, vous devrez ajouter certaines informations pour ce faire, tapez la commande
+
+
+```bash
+  dmesg
+```
+
+
+alors vous pourrez trouver un nouveau disque sûrement nommé sda2 ou sd3....
+
+une fois le nom de votre disque récupéré, un nouveau dossier a été créé
+
+
+```bash
+  sudo mkdir /home/shares/public/disk2
+```
+Alors
+
+```bash
+  sudo mount /dev/LE_NOM_DE_VOTRE_DISQUE /home/shares/public/disk2
+```
+
+
+après avoir créé le dossier entré dans le fichier fstab ATTENTION faites attention à ne pas vous tromper ou bloquer votre raspberry et devoir tout recommencer
+
+```bash
+  sudo nano /etc/fstab
+```
+une fois dans ce fichier copiez et collez à nouveau cette ligne n'oubliez pas les espaces
+
+```bash
+  /dev/LE_NOM_DE_VOTRE_DISQUE /home/shares/public/disk2 auto noatime 0 0
+```
+et ctrl+x , y
+
+fini!!!
---
+++ getSambaScript.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# Vérifier si Samba est installé
+if ! command -v smbd &> /dev/null; then
+    echo "Samba n'est pas installé. Installation en cours..."
+    sudo apt-get update
+    sudo apt-get install -y samba samba-common-bin
+    sudo cp smb.conf /etc/samba/smb.conf
+    sudo mkdir  /home/shares
+    sudo mkdir /home/shares/public
+    sudo chown -R root:users /home/shares/public
+    sudo chmod -R ug=rwx,o=rx /home/shares/public
+    sudo touch /etc/samba/smbpasswd
+    sudo /etc/init.d/smbd restart
+    echo "Samba installé, les dossiers partagés sont dans /home/shares/public."
+
+else
+    echo "que faire ?"
+    echo "1. Désinstaller Samba"
+    echo "2. Ajouter un utilisateur"
+    echo "3. Ajouter un disque partagé"
+    echo "4. voir les dossiers partagés"
+    echo "5. voir les utilisateurs et les mots de passe"
+    echo "6. Quitter"
+fi
+
+read -p "Votre choix : " choix
+
+if [ $choix -eq 1 ]; then
+    echo "Désinstallation de Samba..."
+    sudo apt-get remove -y samba samba-common-bin
+    echo "Désinstallation terminée."
+    exit 0
+elif [ $choix -eq 2 ]; then
+    echo "Ajout d'un utilisateur..."
+    read -p "Nom de l'utilisateur : " user
+    sudo useradd $user -m -G users
+    read -p "Mot de passe : " password
+    sudo smbpasswd -a $user
+    echo "$user:$password" >> /etc/samba/smbpasswd
+    echo "Utilisateur ajouté."
+    exit 0
+elif [ $choix -eq 3 ]; then
+    echo "Ajout d'un disque partagé..."
+    read -p "Nom du disque (exemple disk1=sda1 / disk2 =sda2): " disk
+    sudo mkdir /home/shares/public/$disk
+    #verifier si le disque a un chiffre dans son nom si oui alors on separer le nom du disque et le chiffre pour pouvoir le mettre a sda
+    if [[ $disk =~ [0-9] ]]; then
+        diskName=$(echo $disk | cut -d [0-9] -f1)
+        diskNumber=$(echo $disk | cut -d [0-9] -f2)
+        sda="sda$diskNumber"
+    fi
+    sudo mount /dev/$sda /home/shares/public/$disk
+
+    sudo cp fstab /etc/fstab
+    exit 0
+elif [ $choix -eq 4 ]; then
+    echo "Liste des dossiers partagés :"
+    sudo cat /etc/samba/smb.conf | grep "\[" | cut -d "[" -f2 | cut -d "]" -f1
+    exit 0
+
+elif [ $choix -eq 5 ]; then
+    echo "Liste des utilisateurs et des mots de passe :"
+    sudo cat /etc/samba/smbpasswd
+    exit 0
+else
+    echo "Choix invalide."
+    exit 1
+fi
+
+# Démarrer le service Samba
+sudo service smbd start
+
+echo "Script terminé."
